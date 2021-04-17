7d8f27d0667dc779fc62f99c12fc9d41a26e734c
w25852s <w25852s@126.com>
2021-04-17 14:50:20 +0800

    初始化

---
+++ gulimail-coupon/src/main/java/com/wangshuai/gulimail/lcoupon/GulimailCouponApplication.java
@@ -0,0 +1,13 @@
+package com.wangshuai.gulimail.lcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailCouponApplication.class, args);
+    }
+
+}
---
+++ gulimail-coupon/src/test/java/com/wangshuai/gulimail/lcoupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangshuai.gulimail.lcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
